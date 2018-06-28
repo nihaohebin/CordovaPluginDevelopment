f586c10a31f1439d4fdb957d0514e0a9d97b60b7
hebin <13631284436@163.com>
2018-06-28 09:30:22 +0800

    保存

--- app/src/main/assets/www/cordova_plugins.js
+++ app/src/main/assets/www/cordova_plugins.js
@@ -7,22 +7,13 @@ module.exports = [
     "clobbers": [
       "cordova.getAppVersion"
     ]
-  },
-  {
-    "id": "cordova-plugin-device.device",
-    "file": "plugins/cordova-plugin-device/www/device.js",
-    "pluginId": "cordova-plugin-device",
-    "clobbers": [
-      "device"
-    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-app-version": "0.1.9",
-  "cordova-plugin-device": "2.0.1"
+  "cordova-plugin-app-version": "0.1.9"
 };
 // BOTTOM OF METADATA
 });
