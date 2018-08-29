8c6a14aa78c5775209270ba773d063a3b0f01aee
hebin <13631284436@163.com>
2018-08-29 17:09:28 +0800

    添加jks

--- app/src/main/assets/www/cordova_plugins.js
+++ app/src/main/assets/www/cordova_plugins.js
@@ -1,19 +1,11 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
-  {
-    "id": "cordova-plugin-app-version.AppVersionPlugin",
-    "file": "plugins/cordova-plugin-app-version/www/AppVersionPlugin.js",
-    "pluginId": "cordova-plugin-app-version",
-    "clobbers": [
-      "cordova.getAppVersion"
-    ]
-  }
+
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-app-version": "0.1.9"
 };
 // BOTTOM OF METADATA
 });
---
+++ app/src/main/java/com/toone/hello/MainApplication.java
@@ -0,0 +1,15 @@
+package com.toone.hello;
+
+import android.app.Application;
+
+/**
+ * @author Joker
+ * createTime 2018/8/29 0029
+ * Description
+ */
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
